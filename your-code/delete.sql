ea31e49482054e85c48e835909c05672cb6670bf
juanmolera <juan.molera@gmail.com>
2023-06-07 17:16:41 +0200

    🐬💦💀

---
+++ your-code/delete.sql
@@ -0,0 +1,2 @@
+DELETE FROM cars
+WHERE model = 'Volvo';
---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE salespersons
+SET store  = 'Miami'
+WHERE staffid = 00005;
+
+UPDATE customers
+SET email = 'ppicasso@gmail.com'
+WHERE idcustomer = 10001;
+
+UPDATE customers
+SET email = 'lincoln@us.gov' 
+WHERE idcustomer = 20001;
+
+UPDATE customers
+SET email = 'hello@napoleon.me' 
+WHERE idcustomer = 30001;
