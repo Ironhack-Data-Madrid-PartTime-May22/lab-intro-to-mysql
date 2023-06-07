2f9ff81c08182ccd94ce4c1dca5e06f22368de5a
veronicamonper <veronicamoncadap@gmail.com>
2023-06-07 15:03:38 +0200

    salesperson

---
+++ your-code/salesperson.sql
@@ -0,0 +1,13 @@
+SELECT * FROM lab_seeding.salesperson;
+
+INSERT INTO salesperson (staffid, name, store)
+VALUES (00001, 'Petey Cruiser', 'Madrid'),
+(00002, 'Anna Sthesia', 'Barcelona'),
+(00003, 'Paul Molive', 'Berlin'),
+(00004, 'Gail Forcewind', 'Paris'),
+(00005, 'Paige Turner', 'Miami'),
+(00006, 'Bob Frapples', 'Mexico City'),
+(00007, 'Walter Melon', 'Amsterdam'),
+(00008, 'Shonda Leer', 'Sao Paulo');
+
+SELECT * FROM salesperson;
