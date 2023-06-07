29591a90d5125791dfd49f5102c4494b8a2adbba
veronicamonper <veronicamoncadap@gmail.com>
2023-06-07 14:08:03 +0200

    customererror

---
+++ your-code/seeding_customer.sql
@@ -0,0 +1,7 @@
+SELECT * FROM customers;
+
+INSERT INTO customers (idcustomer, name, phone, email, address, city, province, country, postal);
+VALUES (10001, 'Pablo Picasso', '636 17 63 82', 'null', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', 2845),
+(20001, 'Abraham Lincoln', '1 305 907 7086', 'null', '120 SW 8th St', 'Miami', 'Florida', 'United States', 33130),
+(30001, 'Napoleon Bonaparte', '33 1 79 75 40 00', 'null', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', 75008);
+
